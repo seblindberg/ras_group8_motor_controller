4b15238ec6ad017beaa7ada638168db92800a88e
Sebastian Lindberg <seb.lindberg@gmail.com>
2017-10-04 16:12:27 +0200

    Added loop rate limiting

--- src/motor_controller_node.cpp
+++ src/motor_controller_node.cpp
@@ -5,9 +5,15 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "motor_controller");
   ros::NodeHandle nodeHandle("~");
+  ros::Rate loop_rate(10.0);
   
   ras_group8_motor_controller::MotorController motorController(nodeHandle);
-    
-  ros::spin();
+  
+  //ros::spin();
+  for (;;) {
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  
   return 0;
 }
